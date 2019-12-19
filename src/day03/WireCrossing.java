9aeff9d7cb55c9a1683bc779c0dd13c5dfebf2bf
Ryan Goodwin <34293941+CGBassPlayer@users.noreply.github.com>
2019-12-19 10:12:44 -0500

    Add logging for testing; Add print testing

--- src/day03/WireCrossing.java
+++ src/day03/WireCrossing.java
@@ -2,51 +2,79 @@ package day03;
 
 import common.FileLoader;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class WireCrossing {
     public static void main(String[] args) {
-        ArrayList<String> wireADirections = FileLoader.loadSingleLineCommaStringFile("inputfiles/day03.dat", 1);
-        ArrayList<String> wireBDirections = FileLoader.loadSingleLineCommaStringFile("inputfiles/day03.dat", 2);
+        System.out.println("Wire A");
+        ArrayList<int[]> wireAPositions = getWirePositions(FileLoader.loadSingleLineCommaStringFile("inputfiles/day03Tom.dat", 1));
+        //ArrayList<int[]> wireBPositions = getWirePositions(FileLoader.loadSingleLineCommaStringFile("inputfiles/day03.dat", 2));
 
-        findClosestCrossing(wireADirections, wireBDirections);
+
+
+        try {
+            // Write ArrayList to log file for testing
+            PrintWriter writer = new PrintWriter("Day03.log", "UTF-8");
+            writer.println("x, y");
+            for (int[] wireAPosition : wireAPositions) {
+//                System.out.println(wireAPosition[0] + ", " + wireAPosition[1]);
+                writer.println(wireAPosition[0] + ", " + wireAPosition[1]);
+            }
+            writer.close();
+//            // Get Console output
+//            PrintStream out = new PrintStream(
+//                    new FileOutputStream("Day03.log", true), true);
+//            System.setOut(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void findClosestCrossing(ArrayList<String> wireA, ArrayList<String> wireB) {
-        ArrayList<int[]> wireAPositions = new ArrayList<>();
+    public static ArrayList<int[]> getWirePositions(ArrayList<String> wire) {
+        ArrayList<int[]> wirePositions = new ArrayList<>();
         ArrayList<int[]> tempPos = new ArrayList<>();
-        ArrayList<int[]> wireBPositions = new ArrayList<>();
-        tempPos.add(new int[] {0, 0});
+        tempPos.add(new int[]{0, 0});
+        int size;
 
         // Get all coordinates of
-        for (String s : wireA) {
+        for (String s : wire) {
+            size = Math.max(tempPos.size() - 1, 0);
+
             switch (s.charAt(0)) {
                 case 'U':
-                    for (int i = wireAPositions.size(); i < wireAPositions.size() + Integer.parseInt(s.substring(1)); i++) {
-                        tempPos.add(new int[] {tempPos.get(i)[0], (tempPos.get(i)[1] + 1)});
+                    System.out.println("Up " + s.substring(1));
+                    for (int i = size; i < Integer.parseInt(s.substring(1)); i++) {
+                        tempPos.add(new int[]{tempPos.get(i)[0], (tempPos.get(i)[1] + 1)});
                     }
                     break;
                 case 'D':
-                    for (int i = wireAPositions.size(); i < wireAPositions.size() + Integer.parseInt(s.substring(1)); i++) {
-                        tempPos.add(new int[] {tempPos.get(i)[0], (tempPos.get(i)[1] - 1)});
+                    System.out.println("Down " + s.substring(1));
+                    for (int i = size; i < Integer.parseInt(s.substring(1)); i++) {
+                        tempPos.add(new int[]{tempPos.get(i)[0], (tempPos.get(i)[1] - 1)});
                     }
                     break;
                 case 'L':
-                    for (int i = wireAPositions.size(); i < wireAPositions.size() + Integer.parseInt(s.substring(1)); i++) {
-                        tempPos.add(new int[] {tempPos.get(i)[0] + 1, (tempPos.get(i)[1])});
+                    System.out.println("Left " + s.substring(1));
+                    for (int i = size; i < Integer.parseInt(s.substring(1)); i++) {
+                        tempPos.add(new int[]{tempPos.get(i)[0] + 1, (tempPos.get(i)[1])});
                     }
                     break;
                 case 'R':
-                    for (int i = wireAPositions.size(); i < wireAPositions.size() + Integer.parseInt(s.substring(1)); i++) {
-                        tempPos.add(new int[] {tempPos.get(i)[0] - 1, (tempPos.get(i)[1])});
+                    System.out.println("Right " + s.substring(1));
+                    for (int i = size; i < Integer.parseInt(s.substring(1)); i++) {
+                        tempPos.add(new int[]{tempPos.get(i)[0] - 1, (tempPos.get(i)[1])});
                     }
                     break;
             }
+
+            for (int[] coords : tempPos) {
+                wirePositions.add(new int[]{coords[0], coords[1]});
+            }
+            tempPos.clear();
+            tempPos.add(new int[]{wirePositions.get(wirePositions.size() - 1)[0], wirePositions.get(wirePositions.size() - 1)[1]});
         }
 
-        wireAPositions.addAll(tempPos);
-        for (int[] wireAPosition : wireAPositions) {
-            System.out.println(wireAPosition[0] + ", " + wireAPosition[1]);
-        }
+        return wirePositions;
     }
 }
