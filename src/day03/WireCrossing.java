9384922dbf32d4aa24844d7cf3f28d2335aeba50
Ryan Goodwin <34293941+CGBassPlayer@users.noreply.github.com>
2019-12-19 13:15:26 -0500

    Complete Day 3 Part 1; update gitignore

--- src/day03/WireCrossing.java
+++ src/day03/WireCrossing.java
@@ -4,31 +4,33 @@ import common.FileLoader;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class WireCrossing {
     public static void main(String[] args) {
-        System.out.println("Wire A");
-        ArrayList<int[]> wireAPositions = getWirePositions(FileLoader.loadSingleLineCommaStringFile("inputfiles/day03Tom.dat", 1));
-        //ArrayList<int[]> wireBPositions = getWirePositions(FileLoader.loadSingleLineCommaStringFile("inputfiles/day03.dat", 2));
+        ArrayList<int[]> wireAPositions = getWirePositions(FileLoader.loadSingleLineCommaStringFile("inputfiles/day03.dat", 1));
+        ArrayList<int[]> wireBPositions = getWirePositions(FileLoader.loadSingleLineCommaStringFile("inputfiles/day03.dat", 2));
 
+//        // Write ArrayLists to log file for testing
+//        try {
+//            PrintWriter writer = new PrintWriter("logs/Day03.log", "UTF-8");
+//            writer.println("x, y");
+//            writer.println("Wire A");
+//            for (int[] wireAPosition : wireAPositions) {
+//                writer.println(wireAPosition[0] + ", " + wireAPosition[1]);
+//            }
+//            writer.println("Wire B");
+//            for (int[] wireBPosition : wireBPositions) {
+//                writer.println(wireBPosition[0] + ", " + wireBPosition[1]);
+//            }
+//            writer.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
+        ArrayList<Integer> distances = getDistances(findAllCrossings(wireAPositions, wireBPositions));
 
-        try {
-            // Write ArrayList to log file for testing
-            PrintWriter writer = new PrintWriter("Day03.log", "UTF-8");
-            writer.println("x, y");
-            for (int[] wireAPosition : wireAPositions) {
-//                System.out.println(wireAPosition[0] + ", " + wireAPosition[1]);
-                writer.println(wireAPosition[0] + ", " + wireAPosition[1]);
-            }
-            writer.close();
-//            // Get Console output
-//            PrintStream out = new PrintStream(
-//                    new FileOutputStream("Day03.log", true), true);
-//            System.setOut(out);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Shortest distance: " + Collections.min(distances));
     }
 
     public static ArrayList<int[]> getWirePositions(ArrayList<String> wire) {
@@ -43,27 +45,27 @@ public class WireCrossing {
 
             switch (s.charAt(0)) {
                 case 'U':
-                    System.out.println("Up " + s.substring(1));
+                    //System.out.println("Up " + s.substring(1));
                     for (int i = size; i < Integer.parseInt(s.substring(1)); i++) {
                         tempPos.add(new int[]{tempPos.get(i)[0], (tempPos.get(i)[1] + 1)});
                     }
                     break;
                 case 'D':
-                    System.out.println("Down " + s.substring(1));
+                    //System.out.println("Down " + s.substring(1));
                     for (int i = size; i < Integer.parseInt(s.substring(1)); i++) {
                         tempPos.add(new int[]{tempPos.get(i)[0], (tempPos.get(i)[1] - 1)});
                     }
                     break;
                 case 'L':
-                    System.out.println("Left " + s.substring(1));
+                    //System.out.println("Left " + s.substring(1));
                     for (int i = size; i < Integer.parseInt(s.substring(1)); i++) {
-                        tempPos.add(new int[]{tempPos.get(i)[0] + 1, (tempPos.get(i)[1])});
+                        tempPos.add(new int[]{tempPos.get(i)[0] - 1, (tempPos.get(i)[1])});
                     }
                     break;
                 case 'R':
-                    System.out.println("Right " + s.substring(1));
+                    //System.out.println("Right " + s.substring(1));
                     for (int i = size; i < Integer.parseInt(s.substring(1)); i++) {
-                        tempPos.add(new int[]{tempPos.get(i)[0] - 1, (tempPos.get(i)[1])});
+                        tempPos.add(new int[]{tempPos.get(i)[0] + 1, (tempPos.get(i)[1])});
                     }
                     break;
             }
@@ -76,5 +78,27 @@ public class WireCrossing {
         }
 
         return wirePositions;
+    }
+
+    public static ArrayList<int[]> findAllCrossings(ArrayList<int[]> wireA, ArrayList<int[]> wireB) {
+        ArrayList<int[]> crossingPoints = new ArrayList<>();
+        for (int[] a : wireA) {
+            if (a[0] != 0 && a[1] != 0) {
+                for (int[] b : wireB) {
+                    if (a[0] == b[0] && a[1] == b[1]) {
+                        crossingPoints.add(new int[]{a[0], a[1]});
+                    }
+                }
+            }
+        }
+        return crossingPoints;
+    }
+
+    public static ArrayList<Integer> getDistances(ArrayList<int[]> crossings) {
+        ArrayList<Integer> distances = new ArrayList<>();
+        for (int[] crossing : crossings) {
+            distances.add(Math.abs(crossing[0]) + Math.abs(crossing[1]));
+        }
+        return distances;
     }
 }
