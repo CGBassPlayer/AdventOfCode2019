4d8aa2002c09810364faca4d49aa26b9cbccab98
Ryan Goodwin <34293941+CGBassPlayer@users.noreply.github.com>
2019-12-20 15:44:11 -0500

    Start Day 4 Part 2

--- src/day04/SecureContainerPart2.java
+++ src/day04/SecureContainerPart2.java
@@ -4,7 +4,7 @@ import common.FileLoader;
 
 import java.util.ArrayList;
 
-public class SecureContainer {
+public class SecureContainerPart2 {
     public static void main(String[] args) {
         ArrayList<Integer> range = FileLoader.loadNewLineIntFile("inputfiles/day04.dat");
         ArrayList<Integer> solutions = getPossibleSolutions(range.get(0), range.get(1));
@@ -14,38 +14,25 @@ public class SecureContainer {
     public static ArrayList<Integer> getPossibleSolutions(int low, int high) {
         ArrayList<Integer> totalPasswords = new ArrayList<>();
         for (int i = low; i < high; i++) {
-            if (hasAscendingDigits(i) && containsADouble(i)) {
+            if (SecureContainer.hasAscendingDigits(i) && SecureContainer.containsADouble(i) && hasDoubles(i)) {
+                System.out.println(i);
                 totalPasswords.add(i);
             }
         }
         return totalPasswords;
     }
 
-    public static boolean hasAscendingDigits(int number) {
-        String num = String.valueOf(number);
-        for (int i = 1; i < num.length() - 1; i++) {
-            int j = Character.digit(num.charAt(i - 1), 10);
-            int k = Character.digit(num.charAt(i), 10);
-            int l = Character.digit(num.charAt(i + 1), 10);
-
-            if (j > k || k > l) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static boolean containsADouble(int number) {
+    public static boolean hasDoubles(int number) {
         String num = String.valueOf(number);
         for (int i = 1; i < num.length() - 1; i++) {
             int preDigit = Character.digit(num.charAt(i - 1), 10);
             int digit = Character.digit(num.charAt(i), 10);
             int postDigit = Character.digit(num.charAt(i + 1), 10);
 
-            if (preDigit == digit || digit == postDigit) {
-                return true;
+            if (preDigit == digit && digit == postDigit) {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 }
