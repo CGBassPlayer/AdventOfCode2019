f433763444c3df68dd26e303732a4c963a3527c9
Ryan Goodwin <34293941+CGBassPlayer@users.noreply.github.com>
2019-12-20 11:01:55 -0500

    Update WireCrossing to use Point2D object

--- src/day03/WireCrossing.java
+++ src/day03/WireCrossing.java
@@ -1,8 +1,8 @@
 package day03;
 
 import common.FileLoader;
+import javafx.geometry.Point2D;
 
-import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -11,23 +11,6 @@ public class WireCrossing {
         ArrayList<int[]> wireAPositions = getWirePositions(FileLoader.loadSingleLineCommaStringFile("inputfiles/day03.dat", 1));
         ArrayList<int[]> wireBPositions = getWirePositions(FileLoader.loadSingleLineCommaStringFile("inputfiles/day03.dat", 2));
 
-//        // Write ArrayLists to log file for testing
-//        try {
-//            PrintWriter writer = new PrintWriter("logs/Day03.log", "UTF-8");
-//            writer.println("x, y");
-//            writer.println("Wire A");
-//            for (int[] wireAPosition : wireAPositions) {
-//                writer.println(wireAPosition[0] + ", " + wireAPosition[1]);
-//            }
-//            writer.println("Wire B");
-//            for (int[] wireBPosition : wireBPositions) {
-//                writer.println(wireBPosition[0] + ", " + wireBPosition[1]);
-//            }
-//            writer.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
         ArrayList<Integer> distances = getDistances(findAllCrossings(wireAPositions, wireBPositions));
 
         System.out.println("Shortest distance: " + Collections.min(distances));
@@ -35,46 +18,35 @@ public class WireCrossing {
 
     public static ArrayList<int[]> getWirePositions(ArrayList<String> wire) {
         ArrayList<int[]> wirePositions = new ArrayList<>();
-        ArrayList<int[]> tempPos = new ArrayList<>();
-        tempPos.add(new int[]{0, 0});
-        int size;
+        Point2D point = new Point2D(0, 0);
 
-        // Get all coordinates of
         for (String s : wire) {
-            size = Math.max(tempPos.size() - 1, 0);
-
             switch (s.charAt(0)) {
                 case 'U':
-                    //System.out.println("Up " + s.substring(1));
-                    for (int i = size; i < Integer.parseInt(s.substring(1)); i++) {
-                        tempPos.add(new int[]{tempPos.get(i)[0], (tempPos.get(i)[1] + 1)});
+                    for (int i = 0; i < Integer.parseInt(s.substring(1)); i++) {
+                        point = point.add(0, 1);
+                        wirePositions.add(new int[]{(int) point.getX(), (int) point.getY()});
                     }
                     break;
                 case 'D':
-                    //System.out.println("Down " + s.substring(1));
-                    for (int i = size; i < Integer.parseInt(s.substring(1)); i++) {
-                        tempPos.add(new int[]{tempPos.get(i)[0], (tempPos.get(i)[1] - 1)});
+                    for (int i = 0; i < Integer.parseInt(s.substring(1)); i++) {
+                        point = point.subtract(0, 1);
+                        wirePositions.add(new int[]{(int) point.getX(), (int) point.getY()});
                     }
                     break;
                 case 'L':
-                    //System.out.println("Left " + s.substring(1));
-                    for (int i = size; i < Integer.parseInt(s.substring(1)); i++) {
-                        tempPos.add(new int[]{tempPos.get(i)[0] - 1, (tempPos.get(i)[1])});
+                    for (int i = 0; i < Integer.parseInt(s.substring(1)); i++) {
+                        point = point.subtract(1, 0);
+                        wirePositions.add(new int[]{(int) point.getX(), (int) point.getY()});
                     }
                     break;
                 case 'R':
-                    //System.out.println("Right " + s.substring(1));
-                    for (int i = size; i < Integer.parseInt(s.substring(1)); i++) {
-                        tempPos.add(new int[]{tempPos.get(i)[0] + 1, (tempPos.get(i)[1])});
+                    for (int i = 0; i < Integer.parseInt(s.substring(1)); i++) {
+                        point = point.add(1, 0);
+                        wirePositions.add(new int[]{(int) point.getX(), (int) point.getY()});
                     }
                     break;
             }
-
-            for (int[] coords : tempPos) {
-                wirePositions.add(new int[]{coords[0], coords[1]});
-            }
-            tempPos.clear();
-            tempPos.add(new int[]{wirePositions.get(wirePositions.size() - 1)[0], wirePositions.get(wirePositions.size() - 1)[1]});
         }
 
         return wirePositions;
